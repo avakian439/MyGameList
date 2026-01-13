a58f7a636bcb7ecfc06a1ecaf74275607d4a8277
avakian439 <171086043+avakian439@users.noreply.github.com>
2026-01-13 22:39:52 +0200

    temporary changes to main page navbar

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,10 @@ import { useState, useEffect,useRef } from 'react';
 import {searchGames, getPopularGames, getTopRatedGames, type Game } from '@/lib/rawg-api';
 import Link from 'next/link';
 import { Gamepad2, Search} from 'lucide-react';
+import { UserButton, useUser } from '@clerk/nextjs';
+
 export default function HomePage() {
+    const { isSignedIn } = useUser();
     const[searchQuery, setSearchQuery] = useState('');
     const[searchResult, setSearchResult] = useState<Game[]>([]);
     const[popularGames, setPopularGames] = useState<Game[]>([]);
@@ -17,26 +20,44 @@ export default function HomePage() {
             <nav className="border-b border-gray-800 bg-gray-900/80 backdrop-blur-sm sticky top-0 z-50">
                 <div className="container mx-auto px-4 py-4">
                     <div className="flex flex-col justify-between gap-4">
-                        <Link href="/" className="flex items-center space-x-3 group">
-                            <div className="p-2 bg-gradient-to-br from-purple-600 to-blue-600 rounded-lg group-hover:scale-105 transition-transform">
-                                <Gamepad2 size={28}/>
-                            </div>
-                            <div>
-                                <h1 className="text-2xl font-bold tracking-tight">MYGAME<span className="text-purple-400">List</span></h1>
-                                <p className="text-gray-400 text-sm font-bold">Track your gaming collection</p>
-                            </div>
-                        </Link>
-                        <div className="relative flex-1 max-w-2xl mx-auto">
-                            <div className="relative">
-                                <Search className="absolute  left-4 top-1/2 transform -translate-y-1/2 text-gray-400" size={20} />
-                                <input
-                                    type="text"
-                                    value={searchQuery}
-                                    onChange={(e) => setSearchQuery(e.target.value)}
-                                    placeholder="Search for games..."
-                                    className="w-full pl-12 pr-4 py-3 bg-gray-800 border border-gray-700 rounded-xl focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent transition placeholder-gray-500"
-                                />
+                        <div className="flex">
+                            {/* Logo */}
+                            <Link href="/" className="flex flex-1 items-center space-x-3 group">
+                                <div className="p-2 bg-gradient-to-br from-purple-600 to-blue-600 rounded-lg group-hover:scale-105 transition-transform">
+                                    <Gamepad2 size={28}/>
+                                </div>
+                                <div>
+                                    <h1 className="text-2xl font-bold tracking-tight">MYGAME<span className="text-purple-400">List</span></h1>
+                                    <p className="text-gray-400 text-sm font-bold">Track your gaming collection</p>
+                                </div>
+                            </Link>
+                            {/* Search */}
+                            <div className="relative flex flex-2 max-w-2xl mx-auto justify-center">
+                                <div className="relative">
+                                    <Search className="absolute  left-4 top-1/2 transform -translate-y-1/2 text-gray-400" size={20} />
+                                    <input
+                                        type="text"
+                                        value={searchQuery}
+                                        onChange={(e) => setSearchQuery(e.target.value)}
+                                        placeholder="Search for games..."
+                                        className="w-full pl-12 pr-4 py-3 bg-gray-800 border border-gray-700 rounded-xl focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent transition placeholder-gray-500"
+                                    />
 
+                                </div>
+                            </div>
+                            {/* Acccount Management */}
+                            <div className="flex-1 flex justify-end">
+                                {isSignedIn ? (
+                                    <>
+                                        <Link href="/dashboard" className='ml-5 mr-5 self-center hover:text-purple-400 transition'>Dashboard</Link>
+                                        <UserButton afterSignOutUrl="/"/>
+                                    </>
+                                ) : (
+                                    <>
+                                        <Link href="/sign-up" className='ml-5 self-center hover:text-purple-400 transition'>Sign up</Link>
+                                        <Link href="/sign-in" className='ml-5 mr-5 self-center hover:text-purple-400 transition'>Sign in</Link>
+                                    </>
+                                )}
                             </div>
                         </div>
                     </div>
